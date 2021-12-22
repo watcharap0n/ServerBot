b5dfd3e7a052d30962d06f1fdd1f38d982167b36
watcharap0n <wera.watcharapon@gmail.com>
2021-12-22 16:18:40 +0700

    api intent methods PUT, DELETE

--- server/routers/intents.py
+++ server/routers/intents.py
@@ -27,37 +27,60 @@ class TokenUser(Intent):
 def get_intent(access_token):
     user = db.find_one(collection=collection,
                        query={'access_token': access_token})
+    if not user:
+        return False
     user = TokenUser(**user)
     return user
 
 
 async def verify_name_intent(intent: Intent):
-    user = db.find(collection=collection, query={'uid': intent.name})
+    user = get_intent(intent.access_token)
     if user:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail='Invalid value key name duplicate')
-    user = TokenUser(**user)
-    return user
+        if user.name == intent.name:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail='Invalid value key name duplicate')
+    return intent
 
 
 @router.post('/create', response_model=TokenUser)
-async def create_intent(item_model: dict = Depends(verify_name_intent),
-                        current_user: User = get_current_active):
+async def create_intent(intent: Intent = Depends(verify_name_intent),
+                        current_user: User = Depends(get_current_active)):
     Id = generate_token(engine=ObjectId())
+    item_model = jsonable_encoder(intent)
     item_model['id'] = Id
-    item_model['uid'] = current_user.uid
+    item_model['uid'] = current_user.data.uid
     db.insert_one(collection=collection, data=item_model)
     store_model = TokenUser(**item_model)
     return store_model
 
 
-@router.get('/get', response_model=TokenUser)
-async def get_intents(access_token: Optional[str] = None):
-    user = get_intent(access_token)
+@router.get('/get')
+async def get_intents(access_token: Optional[str] = None,
+                      current_user: User = Depends(get_current_active)):
+    user = db.find(collection=collection, query={'access_token': access_token})
+    user = list(user)
     if not user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail='Not found intents')
     return user
 
 
+@router.put('/query/update/{id}')
+async def update_intent(id: str, intent: Intent,
+                        current_user: User = Depends(get_current_active)):
+    data = jsonable_encoder(intent)
+    query = {'id': id}
+    values = {'$set': data}
+    db.update_one(collection=collection, query=query, values=values)
+    return {'detail': f'Update success {id}'}
 
+
+@router.delete('/query/delete/{id}')
+async def delete_intent(id: str,
+                        current_user: User = Depends(get_current_active)):
+    try:
+        db.delete_one(collection=collection, query={'id': id})
+        return {'detail': f'Delete success {id}'}
+    except Exception:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail='Invalid data message')
