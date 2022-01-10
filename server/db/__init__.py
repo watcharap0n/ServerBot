254660c9d48ab0298ef4c86f336960910eed9152
watcharap0n <wera.watcharapon@gmail.com>
2022-01-11 00:29:13 +0700

    update func generate token

--- server/db/__init__.py
+++ server/db/__init__.py
@@ -13,6 +13,7 @@ from .environ.client_firebase import firebaseConfig, firebaseAuth
 from .database import MongoDB
 from .object_str import CutId, PyObjectId
 
+
 client = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
 db = MongoDB(database_name='MangoBOT', uri=client)
 
@@ -23,6 +24,6 @@ def generate_token(engine):
     :param engine:
     :return:
     """
-    obj = object_str.CutId(_id=engine)
+    obj = CutId(_id=engine)
     Id = obj.dict()['id']
     return Id
