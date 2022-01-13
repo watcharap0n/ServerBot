b31049a7589e88458a83c0f238ce8b7df574c36e
watcharap0n <wera.watcharapon@gmail.com>
2022-01-13 10:46:01 +0700

    config nuxt.js

---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+module.exports = {
+    ssr: false,
+    head: {
+        titleTemplate: '%s Platform CHATBOT',
+        meta: [
+            {charset: 'utf-8'},
+            {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+            {hid: 'description', name: 'description', content: 'Meta description'}
+        ],
+        script: [
+            {
+                src: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js'
+            }
+        ],
+        link: [
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.googleapis.com'
+            },
+            {
+                rel: 'preconnect',
+                href: 'https://fonts.gstatic.com'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Prompt:wght@300&display=swap'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Ubuntu&display=swap'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap'
+            }
+        ]
+    },
+
+    buildModules: [
+        "@nuxtjs/vuetify"
+    ],
+
+    modules: [
+        "@nuxtjs/axios",
+        "bootstrap-vue/nuxt",
+        "vue-sweetalert2/nuxt"
+    ],
+
+    srcDir: 'app/',
+
+    // css: [
+    //     '~/assets/css/style.css'
+    // ],
+    // styleResources: {
+    //     scss: [
+    //         '~/assets/scss/style.scss',
+    //     ]
+    // },
+    components: true
+}
