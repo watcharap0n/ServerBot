46dd5025e95adfaf8d2542b581a3d6b5458be73f
watcharap0n <wera.watcharapon@gmail.com>
2022-01-13 10:39:52 +0700

    app branch dev

---
+++ app/layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-app style="font-family: 'Prompt', sans-serif;">
+
+      <v-container justify="center" align="center">
+        <Nuxt/>
+      </v-container>
+
+    </v-app>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+</style>
