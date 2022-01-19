49dbb1fc559e902ba343cff5e4b0f1faed67f5db
watcharap0n <wera.watcharapon@gmail.com>
2022-01-19 16:22:27 +0700

    page forgot

---
+++ app/pages/authentication/forgot.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+
+    <div class="p-28 ">
+      <h2 class="text-5xl">
+        ลืมรหัสผ่าน
+      </h2>
+      <br>
+      <p>กรอกอีเมลของคุณ และคุณจะได้รับวิธีการเปลี่ยนรหัสผ่านจากเรา</p>
+
+      <FormSubmit name-btn="ลืมรหัสผ่าน"
+                  name-ele1="ที่อยู่อีเมล"
+                  :submit-form="submitForm"
+      />
+    </div>
+
+
+  <v-overlay :value="spinLoading">
+      <v-progress-circular
+        indeterminate
+        size="64"
+      ></v-progress-circular>
+    </v-overlay>
+
+
+  </div>
+</template>
+
+<script>
+import FormSubmit from "../../components/dynamics/FormSubmit";
+
+export default {
+  components: {FormSubmit},
+  layout: 'empty',
+  data() {
+    return {
+      spinLoading: false
+    }
+  },
+  methods: {
+    async submitForm(infoEle) {
+      this.spinLoading = true
+      const path = `/authentication/forgot?email=${infoEle.ele1}`
+      await this.$axios.get(path)
+          .then((res) => {
+            console.log(res.data)
+            this.$router.push('/authentication')
+          })
+          .catch((err) => {
+            console.error(err)
+          })
+      this.spinLoading = false
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
