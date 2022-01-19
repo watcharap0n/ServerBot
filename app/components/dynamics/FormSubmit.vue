18f9e10c103bd3bd29a723c101948b179a1f773a
watcharap0n <wera.watcharapon@gmail.com>
2022-01-20 01:01:32 +0700

    fix FormSubmit

--- app/components/dynamics/FormSubmit.vue
+++ app/components/dynamics/FormSubmit.vue
@@ -1,31 +1,28 @@
 <template>
-  <div>
+  <v-form>
+    <v-text-field v-model="formEle.ele1"
+                  rounded
+                  filled
+                  :label="nameEle1"
+    >
+    </v-text-field>
 
-    <v-form>
-      <v-text-field v-model="formEle.ele1"
-                    rounded
-                    filled
-                    :label="nameEle1"
-      >
-      </v-text-field>
+    <v-text-field v-model="formEle.ele2"
+                  rounded
+                  filled
+                  v-if="field1"
+                  :label="nameEle2"
+    >
+    </v-text-field>
+    <v-btn color="pink accent-2"
+           rounded
+           v-text="nameBtn"
+           @click="submitForm(formEle)"
+           class="text-white"
+    >
 
-      <v-text-field v-model="formEle.ele2"
-                    rounded
-                    filled
-                    v-if="field1"
-                    :label="nameEle2"
-      >
-      </v-text-field>
-      <v-btn color="pink accent-2"
-             rounded
-             v-text="nameBtn"
-             @click="submitForm(formEle)"
-             class="text-white"
-      >
-
-      </v-btn>
-    </v-form>
-  </div>
+    </v-btn>
+  </v-form>
 </template>
 
 <script>
