cec09271f790f31cd70b3784cc33b23be3a221d2
watcharap0n <wera.watcharapon@gmail.com>
2022-01-20 01:30:28 +0700

    Component APP

---
+++ app/components/app/Button.vue
@@ -0,0 +1,42 @@
+<template>
+
+  <v-btn
+      :icon="icon"
+      class="mx-2"
+      :fab="fab"
+      :dark="dark"
+      :color="color"
+      :small="small"
+      @click="btnSubmit"
+      :text="text"
+      :to="to"
+  >
+    <v-icon dark v-if="haveIcon">
+      {{ nameIcon }}
+    </v-icon>
+    {{ nameBtn }}
+  </v-btn>
+
+</template>
+
+<script>
+export default {
+  props: [
+    'to',
+    'text',
+    'nameBtn',
+    'btnSubmit',
+    'fab',
+    'dark',
+    'haveIcon',
+    'color',
+    'icon',
+    'nameIcon',
+    'size',
+    'small'
+  ]
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/components/layout/AppBar.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-app-bar
+      flat
+      fixed
+      :style="`margin-left: ${$vuetify.application.left}px;`"
+  >
+    <v-toolbar-title>Platform CHATBOT</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+
+    <Button icon="icon" :have-icon="true" name-icon="mdi-heart" color="pink accent-2"/>
+    <Button icon="icon" :have-icon="true" name-icon="mdi-magnify" color="pink accent-2"/>
+    <div class="pa-2" v-if="$auth.loggedIn">
+      <Button name-btn="ออกจากระบบ" text="text" :btn-submit="logout"/>
+    </div>
+
+    <div v-else>
+      <Button to="/authentication" text="text" name-btn="เข้าสู่ระบบ"/>
+    </div>
+  </v-app-bar>
+</template>
+
+<script>
+import Button from "../app/Button";
+export default {
+  components: {Button},
+  methods:{
+    logout() {
+      this.$auth.logout()
+          .then(() => {
+            this.$router.push('/authentication')
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
