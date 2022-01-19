e99ba6f53e4f19dd87088f878367c0ba13b2ddf9
watcharap0n <wera.watcharapon@gmail.com>
2022-01-20 02:21:08 +0700

    update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -76,5 +76,15 @@ module.exports = {
                 }
             }
         }
+    },
+    build: {
+        postcss: {
+            preset: {
+                features: {
+                    // Fixes: https://github.com/tailwindcss/tailwindcss/issues/1190#issuecomment-546621554
+                    "focus-within-pseudo-class": false
+                }
+            }
+        }
     }
 }
