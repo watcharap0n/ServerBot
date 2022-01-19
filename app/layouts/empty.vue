999ccfe08b2375c91d405c6f02756935f807a8ec
watcharap0n <wera.watcharapon@gmail.com>
2022-01-20 01:31:25 +0700

    layout empty.vue

---
+++ app/layouts/empty.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+
+    <v-app style="font-family: 'Kanit', sans-serif;">
+      <Nuxt/>
+    </v-app>
+
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
