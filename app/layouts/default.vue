29c1b49aa912d986de23328a135adcebf1ad849e
watcharap0n <wera.watcharapon@gmail.com>
2022-01-20 01:31:14 +0700

    layout default.vue

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,21 +1,35 @@
 <template>
   <div>
-    <v-app style="font-family: 'Prompt', sans-serif;">
-
-      <v-container justify="center" align="center">
+    <v-app style="font-family: 'Ubuntu', sans-serif;" class="bg-gray-200">
+      <NaviDraw/>
+      <AppBar/>
+      <div
+          v-if="$auth.loggedIn"
+          :style="`margin-left: ${$vuetify.application.left}px; margin-top: ${$vuetify.application.top}`"
+          class="bg-gray-100 h-screen"
+      >
+        <br><br><br>
         <Nuxt/>
-      </v-container>
-
+      </div>
     </v-app>
   </div>
 </template>
 
 <script>
+import AppBar from "../components/layout/AppBar";
+import NaviDraw from "../components/layout/NaviDraw";
+import Button from "../components/app/Button";
 
 export default {
+  components: {AppBar, Button, NaviDraw},
+  props: ['toolbarTitle'],
   data() {
     return {
+
     }
+  },
+  methods: {
+
   }
 }
 
