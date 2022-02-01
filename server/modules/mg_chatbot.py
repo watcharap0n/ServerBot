f4f036cd986fce709196654d42322f5901cb90af
watcharap0n <wera.watcharapon@gmail.com>
2022-02-01 13:26:28 +0700

    mg_chatbot.py

--- server/modules/mg_chatbot.py
+++ server/modules/mg_chatbot.py
@@ -8,12 +8,16 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import SGDClassifier
-from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn.feature_extraction.text import (
+    TfidfVectorizer,
+    CountVectorizer,
+    TfidfTransformer,
+)
 
 
-def chatbot_pipeline(X: list, y: list,
-                     message: str,
-                     X_test: Optional[list] = None) -> dict:
+def chatbot_pipeline(
+    X: list, y: list, message: str, X_test: Optional[list] = None
+) -> dict:
     """
 
     :param message:
@@ -23,26 +27,28 @@ def chatbot_pipeline(X: list, y: list,
     :return:
     """
 
-    X = [re.sub(re.compile(r'\s+'), '', i) for i in X]
-    text_clf_logis = Pipeline([('vect', CountVectorizer(tokenizer=tokenize)),
-                               ('tfidf', TfidfTransformer()),
-                               ('clf-svm', LogisticRegression()),
-                               ])
+    X = [re.sub(re.compile(r"\s+"), "", i) for i in X]
+    text_clf_logis = Pipeline(
+        [
+            ("vect", CountVectorizer(tokenizer=tokenize)),
+            ("tfidf", TfidfTransformer()),
+            ("clf-svm", LogisticRegression()),
+        ]
+    )
     text_clf_logis.fit(X, y)
     if X_test:
         predicted = text_clf_logis.predict(X_test)
         pred_prob = text_clf_logis.predict_proba(X_test)
         acc = np.mean(predicted == y)
-        return {'accuracy': acc, 'pred_prob': pred_prob,
-                'predicted': predicted}
+        return {"accuracy": acc, "pred_prob": pred_prob, "predicted": predicted}
     message = list(message)
     predicted = text_clf_logis.predict(message)
-    return {'predicted': predicted}
+    return {"predicted": predicted}
 
 
-def chatbot_standard(X: list, y: list,
-                     message: str,
-                     X_test: Optional[list] = None) -> dict:
+def chatbot_standard(
+    X: list, y: list, message: str, X_test: Optional[list] = None
+) -> dict:
     """
 
     :param message:
@@ -52,10 +58,10 @@ def chatbot_standard(X: list, y: list,
     :return:
     """
 
-    X = [re.sub(re.compile(r'\s+'), '', i) for i in X]
+    X = [re.sub(re.compile(r"\s+"), "", i) for i in X]
     tf_vect = CountVectorizer(tokenizer=tokenize)
     x_train_vect = tf_vect.fit_transform(X)
-    clf = LogisticRegression(penalty='none')
+    clf = LogisticRegression(penalty="none")
     clf.fit(x_train_vect, y)
     message = list(message)
     x_test_vect = tf_vect.transform(message)
@@ -63,7 +69,6 @@ def chatbot_standard(X: list, y: list,
         predicted = clf.predict(x_test_vect)
         pred_prob = clf.predict_proba(x_test_vect)[0][predicted]
         acc = np.mean(predicted == y)
-        return {'accuracy': acc, 'pred_prob': pred_prob,
-                'predicted': predicted}
+        return {"accuracy": acc, "pred_prob": pred_prob, "predicted": predicted}
     predicted = clf.predict(x_test_vect)
-    return {'predicted': predicted}
+    return {"predicted": predicted}
