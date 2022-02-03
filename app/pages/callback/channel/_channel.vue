0b6294b5e33c3aabe00d4340e09bdba1d5672819
watcharap0n <wera.watcharapon@gmail.com>
2022-02-03 21:06:04 +0700

    _channel.vue

---
+++ app/pages/callback/channel/_channel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-if="$auth.loggedIn">
+      {{ $auth.user }}
+    </div>
+
+    <div v-else>
+      <v-btn to="/authentication">SignIn</v-btn>
+    </div>
+    <h2>Hello </h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+
+    }
+  },
+  created() {
+    console.log(this.$route.params)
+  }
+}
+</script>
+
+<style scoped>
+</style>
