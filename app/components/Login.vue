0fdd02295ba7ddaff78821d60e09b8afb76afbde
watcharap0n <wera.watcharapon@gmail.com>
2022-02-03 21:11:23 +0700

    Component Login.vue

--- app/components/Login.vue
+++ app/components/Login.vue
@@ -1,53 +1,111 @@
 <template>
   <div>
-    <v-form
-        ref="form"
-        v-model="valid"
-        lazy-validation
-    >
-      <v-text-field
-          prepend-inner-icon="mdi-email"
-          color="purple darken-2"
-          :rules="emailRules"
-          v-model="email"
-          label="อีเมล"
-          filled
-          rounded
+    <v-container>
+      <v-img
+          :fullscreen="$vuetify.breakpoint.mobile"
+          position="center"
+          max-height="400"
+          max-width="250"
+          :src="require('~/assets/images/mango-profile.jpg')"
       >
-      </v-text-field>
-      <v-text-field
-          @click:append="show1 = !show1"
-          :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show1 ? 'text' : 'password'"
-          prepend-inner-icon="mdi-lock"
-          color="purple darken-2"
-          :rules="passwordRules"
-          v-model="password"
-          label="รหัสผ่าน"
-          filled
-          rounded
-          @keyup.enter="submitLogin"
-      >
-      </v-text-field>
-      <v-btn
-          block
-          color="pink accent-2"
-          rounded
-          class="text-white"
-          @click="submitLogin"
-          :loading="spinSubmit"
-          :disabled="!valid"
-      >เข้าสู่ระบบ
-      </v-btn>
-    </v-form>
+
+      </v-img>
+      <v-col sm="5">
+        <p class=" text-7xl font-extrabold text-green-500 " style="color: #00838F">Welcome to</p>
+        <p class="text-6xl text-green-500" style="color: #00838F">Platform Chatbot</p>
+        <br>
+        <br>
+        <v-form
+            ref="form"
+            v-model="valid"
+            lazy-validation
+        >
+          <v-text-field
+              style="background-color: #D7F9FA"
+              prepend-inner-icon="mdi-account-outline"
+              :rules="emailRules"
+              v-model="email"
+              label="อีเมล"
+              rounded
+              @keyup.enter="submitLogin"
+          >
+          </v-text-field>
+          <br>
+          <v-text-field
+              style="background-color: #D7F9FA"
+              @click:append="show1 = !show1"
+              :append-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show1 ? 'text' : 'password'"
+              prepend-inner-icon="mdi-lock"
+              :rules="passwordRules"
+              v-model="password"
+              label="รหัสผ่าน"
+              rounded
+              @keyup.enter="submitLogin"
+          >
+          </v-text-field>
+          <br>
+
+          <v-row
+              align="center"
+          >
+            <v-col>
+              <v-radio-group
+                  v-model="radioGroup"
+              >
+                <v-radio
+
+                    label="จดจำฉัน"
+                    color="primary"
+                    value="primary"
+                ></v-radio>
+              </v-radio-group>
+            </v-col>
+
+            <v-col sm="4">
+              <NuxtLink to="/authentication/forgot" class="text-body">
+                ลืมรหัสผ่าน?
+              </NuxtLink>
+            </v-col>
+          </v-row>
+          <br>
+          <v-btn
+              x-large
+              style="width:200px"
+              color="#12AE7E"
+              rounded
+              class="text-white "
+              @click="submitLogin"
+              :loading="spinSubmit"
+              :disabled="!valid"
+          >เข้าสู่ระบบ
+          </v-btn>
+          <br>
+          <br>
+          <br>
+          <v-row
+              justify="center">
+            <p>Don't have an account ?</p>
+            <NuxtLink to="/authentication/register"
+                      style="color: #3BBBBC"
+            >
+              สมัครสมาชิก
+            </NuxtLink>
+          </v-row>
+        </v-form>
+      </v-col>
+    </v-container>
 
   </div>
 </template>
 
 <script>
+
 export default {
+
   data() {
     return {
+      radioGroup: 1,
       show1: false,
       valid: true,
       emailRules: [
@@ -75,8 +133,8 @@ export default {
     loginInfo(formData) {
       this.spinSubmit = true
       this.$auth.loginWith('local', {data: formData})
-          .then((res) => {
-            console.log(res);
+          .then(() => {
+            // console.log(this.$auth.strategy.token.get())
             this.$router.push('/home');
             this.spinSubmit = false
             this.$refs.form.reset();
@@ -102,7 +160,8 @@ export default {
             }
           })
     }
-  }
+  },
+
 }
 </script>
 
