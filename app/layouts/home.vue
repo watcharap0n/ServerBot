3b4529ee7b82fc1731d38dab479a9f11fbe4d24f
watcharap0n <wera.watcharapon@gmail.com>
2022-02-03 21:10:33 +0700

    layout home.vue

---
+++ app/layouts/home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+    <v-app style="font-family: 'Kanit', sans-serif;">
+      <div v-if="$auth.loggedIn">
+        <Nuxt/>
+      </div>
+
+    </v-app>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  middleware: ['auth-admin'],
+}
+</script>
+
+<style scoped>
+</style>
