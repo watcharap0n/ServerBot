02b0b47fc729c82c52595b8c33fd7ac5c0ccf031
watcharap0n <wera.watcharapon@gmail.com>
2022-02-08 12:20:19 +0700

    add Component CreateChannel.vue branch review

---
+++ app/components/app/CreateChannel.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card
+      color="#D7F9FA"
+      class="mx-auto my-12 text-center text-decoration-none"
+      max-width="390"
+      height="400"
+      link
+      to="/create/channel"
+  >
+    <v-card-text>
+      <div class="m-40">
+        <v-icon x-large>mdi-plus</v-icon>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
