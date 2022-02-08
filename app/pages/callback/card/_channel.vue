d8b53efa1bc718ca3d77d60fcd0a20ec6a483257
watcharap0n <wera.watcharapon@gmail.com>
2022-02-08 18:30:52 +0700

    _channel.vue branch review

--- app/pages/callback/card/_channel.vue
+++ app/pages/callback/card/_channel.vue
@@ -1,23 +1,13 @@
 <template>
   <div>
-    <div v-if="$auth.loggedIn">
 
-
-
-    </div>
-
-    <div v-else>
-      <v-btn to="/authentication">SignIn</v-btn>
-    </div>
   </div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-
-    }
+  data() {
+    return {}
   },
   async created() {
     await this.$parent.$emit('routerHandle', this.$route.params);
