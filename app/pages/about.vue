972de4213fb99d633393b182f4b4491d8f9c70ee
watcharap0n <wera.watcharapon@gmail.com>
2022-02-08 13:47:28 +0700

    about.vue

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,54 +1,12 @@
 <template>
+  <div>
 
-  <v-container>
-
-    <v-row>
-      <v-text-field
-          v-model="email"
-          label="Email"
-          solo
-      ></v-text-field>
-      &nbsp;
-      <v-text-field
-          v-model="password"
-          label="password"
-          solo
-      ></v-text-field>
-    </v-row>
-
-    <v-btn color="success" @click="submitData({'email': email, 'password': password})">
-      submit
-    </v-btn>
-
-    <br>
-    <br>
-    <br>
-    This is Data - > {{ objectData }}
-
-  </v-container>
-
+  </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      email: '',
-      password: '',
-      objectData: {}
-    }
-  },
-  methods: {
-    submitData(val) {
-      console.log(val)
-      this.objectData = val
-    }
-  }
-
-}
+export default {}
 </script>
 
-
 <style scoped>
-
 </style>
