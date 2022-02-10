b316e66aa0e1a37ecdce0f05c762ec25b3a2bcaa
watcharap0n <wera.watcharapon@gmail.com>
2022-02-10 17:32:37 +0700

    features.js

---
+++ app/store/features.js
@@ -0,0 +1,75 @@
+export const state = () => ({
+    data: [],
+    response: null,
+    dynamicPath: '',
+})
+
+export const mutations = {
+    payload(state, payload) {
+        state.data = payload
+    },
+    setResponse(state, payload) {
+        state.response = payload
+    },
+    setDynamicPath(state, payload) {
+        state.dynamicPath = payload
+    }
+}
+
+export const actions = {
+    async crateCard(context) {
+        const path = '/card/create';
+        return this.$axios.post(path, context.getters.getPayload)
+            .then((res) => {
+                context.commit('setResponse', res.data)
+            })
+            .catch((err) => {
+                context.commit('setResponse', err.response)
+                this.$notifier.showMessage({
+                    content: `ชื่อการ์ดนี้เคยมีการสร้างแล้ว!`,
+                    color: 'red'
+                })
+            })
+    },
+    async deleteCard(context) {
+        const path = `/card/query/delete/${context.getters.getDynamicPath}`;
+        return this.$axios.delete(path)
+            .then((res) => {
+                context.commit('setResponse', res.data)
+            })
+            .catch((err) => {
+                context.commit('setResponse', err)
+                this.$notifier.showMessage({
+                    content: `มีบางอย่างผิดพลาด status code ${err.response.status}`,
+                    color: 'red'
+                })
+            })
+
+    },
+    async updateCard(context) {
+        const path = `/card/query/update/${context.getters.getDynamicPath}`;
+        return this.$axios.put(path, context.getters.getPayload)
+            .then((res) => {
+                context.commit('setResponse', res.data)
+            })
+            .catch((err) => {
+                this.$notifier.showMessage({
+                    content: `มีบางอย่างผิดพลาด status code ${err.response.status}`,
+                    color: 'red'
+                })
+            })
+    }
+}
+
+
+export const getters = {
+    getPayload(state) {
+        return state.data
+    },
+    getResponse(state) {
+        return state.response
+    },
+    getDynamicPath(state) {
+        return state.dynamicPath
+    }
+}
