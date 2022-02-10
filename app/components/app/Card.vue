24bfb442742641d5c7d0947bc522b0fcf93454f7
watcharap0n <wera.watcharapon@gmail.com>
2022-02-10 17:29:12 +0700

    Card.vue branch review

--- app/components/app/Card.vue
+++ app/components/app/Card.vue
@@ -1,90 +1,99 @@
 <template>
-  <div>
-    <v-container>
-      <v-col sm="7">
-        <v-card class="text-center p-2">
+  <v-card class="text-center p-2">
+    <v-card-text>
+      <v-form ref="formCard"
+              v-model="valid">
+        <p class="text-xl font-normal font-extrabold text-green-500">ชื่อการ์ด</p>
+        <v-text-field
+            color="#12AE7E"
+            prepend-icon="mdi-card-text"
+            v-model="card.name"
+            label="ชื่อการ์ด"
+            filled
+        ></v-text-field>
+        <p class="text-xl font-normal font-extrabold text-green-500">โค้ด</p>
+        <v-textarea
+            color="#12AE7E"
+            prepend-icon="mdi-code-braces"
+            v-model="card.content"
+            label="โค้ด"
+        >
+        </v-textarea>
 
-          <v-form ref="formCard"
-                  v-model="valid">
-            <p class="text-xl font-normal">ชื่อการ์ด</p>
-            <v-text-field
-                v-model="card.card"
-                label="ชื่อการ์ด"
-                filled
-            ></v-text-field>
-            <br>
-            <v-divider class="mx-4"></v-divider>
-            <br>
-            <p class="text-xl font-normal">โค้ด</p>
-            <v-textarea
-                v-model="card.content"
-                label="โค้ด"
-            >
-              <template>
-                <div>
-                  โค้ด <small></small>
-                </div>
-              </template>
-            </v-textarea>
-            <br>
-            <v-divider class="mx-4"></v-divider>
-            <br>
-            <p class="text-xl font-normal">ข้อความ</p>
-            <v-text-field
-                v-model="card.massage"
-                label="ข้อความ"
-                outlined
-            ></v-text-field>
-            <v-row justify="center">
-              <p class="text-center">คุณสามารถไปออกแบบ Card ได้ที่ :</p>
-              <a class="text-center" href="https://developers.line.biz/flex-simulator/">
-                Card
-              </a>
-            </v-row>
-            <br>
-            <v-row justify="end">
-              <v-col sm="5">
-              <v-btn
-                  text
-                  x-large
-              >บันทึกข้อมูล
-              </v-btn>
-              <v-btn
-                  color="red"
-                  fab
-                  dark
-                  small
-              >
-                <v-icon>mdi-delete</v-icon>
-              </v-btn>
-                </v-col>
-            </v-row>
-
-          </v-form>
-
-        </v-card>
-      </v-col>
-    </v-container>
-  </div>
+        <p class="text-xl font-normal font-extrabold text-green-500">อธิบาย</p>
+        <v-text-field
+            color="#12AE7E"
+            prepend-icon="mdi-message-bulleted"
+            v-model="card.message"
+            label="ข้อความ"
+            outlined
+        ></v-text-field>
+        <v-row justify="center">
+          <p class="text-center font-extrabold text-red-400">*คุณสามารถไปออกแบบการ์ดได้ที่:</p> &nbsp;&nbsp;&nbsp;
+          <a class="text-center text-base" href="https://developers.line.biz/flex-simulator/" target="_blank">
+            Flex Message
+          </a>
+        </v-row>
+        <v-row justify="end">
+          <v-btn
+              text
+              color="success"
+              :loading="spin"
+              @click="updateCard"
+          >
+            <v-icon left>mdi-database-plus</v-icon>
+            บันทึกข้อมูล
+          </v-btn>
+          <v-btn
+              :loading="spin"
+              color="red"
+              dark
+              @click="deleteCard"
+              text
+          >
+            <v-icon left>mdi-delete</v-icon>
+            ลบข้อมูล
+          </v-btn>
+        </v-row>
+      </v-form>
+    </v-card-text>
+  </v-card>
 </template>
 
 <script>
 
 export default {
+  props: {
+    deleteCard: {
+      required: false
+    },
+    updateCard: {
+      required: false
+    },
+    setObject: {
+      required: false,
+    },
+    spin: {
+      type: Boolean,
+      required: false,
+      default: false
+    }
+  },
   data() {
     return {
       valid: true,
-      card: {
-        content: "",
-        card: "",
-        massage: "",
-        accessToken: ""
+    }
+  },
+  computed: {
+    card: {
+      get(){
+        return this.setObject;
+      },
+      set(val){
+        return this.$emit('input', val)
       }
-
-
     }
   }
-
 }
 </script>
 
