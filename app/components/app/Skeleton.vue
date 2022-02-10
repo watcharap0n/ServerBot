b921332f6ed174be952aa13e5de34e7dbac5b2b0
watcharap0n <wera.watcharapon@gmail.com>
2022-02-10 17:33:13 +0700

    Skeleton.vue

---
+++ app/components/app/Skeleton.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-sheet
+      v-if="condition"
+      :color="`grey ${theme.isDark ? 'darken-2' : 'lighten-4'}`"
+      class="pa-3"
+  >
+    <v-row>
+      <v-col cols="12"
+             sm="4"
+             v-for="n in 9"
+             :key="n"
+      >
+        <v-skeleton-loader
+            :loading="condition"
+            class="mx-auto"
+            max-width="390"
+            type="card-avatar, article, actions"
+        ></v-skeleton-loader>
+      </v-col>
+    </v-row>
+  </v-sheet>
+</template>
+
+
+<script>
+export default {
+  props: ['condition'],
+  inject: {
+    theme: {
+      default: {isDark: false},
+    },
+  },
+}
+</script>
