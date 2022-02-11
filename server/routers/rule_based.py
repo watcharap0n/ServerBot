ca40e9f61dd1812d61bfba535e7241838d3f7679
watcharap0n <wera.watcharapon@gmail.com>
2022-02-11 15:43:29 +0700

    rule_based.py branch review

--- server/routers/rule_based.py
+++ server/routers/rule_based.py
@@ -18,7 +18,9 @@ async def check_rule_based_duplicate(rule_based: RuleBased):
     )
     items = list(items)
     for item in items:
-        if item["keyword"] == rule_based.keyword:
+        if item['keyword'] is None:
+            return rule_based
+        elif item["keyword"] == rule_based.keyword:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST, detail="item keyword duplicate"
             )
