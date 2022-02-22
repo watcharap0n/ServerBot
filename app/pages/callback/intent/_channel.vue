4c2663e3d0a36443cc25e34c6e9dc8df58c76d3c
watcharap0n <wera.watcharapon@gmail.com>
2022-02-22 15:59:20 +0700

    update lang channel intent branch review

--- app/pages/callback/intent/_channel.vue
+++ app/pages/callback/intent/_channel.vue
@@ -7,7 +7,7 @@
           flat
       >
         <v-icon>mdi-robot</v-icon>&nbsp;
-        <v-toolbar-title>หัวข้อ</v-toolbar-title>
+        <v-toolbar-title>Intent</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn color="info"
                rounded
@@ -15,7 +15,7 @@
                :hidden="!btnShow"
         >
           <v-icon left>mdi-robot</v-icon>
-          เพิ่มหัวข้อ
+          Add Intent
         </v-btn>
 
       </v-toolbar>
@@ -57,7 +57,7 @@
                 class="text-h6 grey--text text--lighten-1 font-weight-light"
                 style="align-self: center;"
             >
-              เลือกรายการ
+              select item
             </div>
             <v-card
                 v-else
@@ -84,7 +84,7 @@
             header="เพิ่มหัวข้อ"
             :element-forms="elements"
             max-width="450"
-            body="กรุณาตั้งชื่อหัวข้อเพิ่มสร้างหัวข้อของท่าน"
+            body="Set name Intent"
             :loading-dialog="!spinSave"
             :submit-dialog="save"
     />
@@ -125,8 +125,8 @@ export default {
       elements: [
         {
           color: 'primary',
-          label: 'ตั้งชื่อหัวข้อ',
-          rules: [v => !!v || 'กรุณากรอกข้อมูล'],
+          label: 'Name Intent',
+          rules: [v => !!v || 'required!'],
           icon: 'mdi-robot',
           value: this.setName
         }
@@ -137,7 +137,7 @@ export default {
     items() {
       return [
         {
-          name: 'สอนบอทของฉัน',
+          name: 'My Intents',
           children: this.users,
         },
       ]
@@ -179,7 +179,7 @@ export default {
           })
           .catch((err) => {
             this.$notifier.showMessage({
-              content: 'มีบางอย่างผิดพลาด',
+              content: `something wrong! ${err.response.status}`,
               color: 'red'
             })
             console.error(err);
@@ -197,7 +197,7 @@ export default {
             this.users.push(this.form)
             this.form = Object.assign({}, this.defaultForm)
             this.$notifier.showMessage({
-              content: 'สร้างหัวข้อสำเร็จ คุณสามารถกำหนดรูปแบบได้แล้ว',
+              content: 'create intent successfully',
               color: 'success'
             })
           })
@@ -205,12 +205,12 @@ export default {
             console.error(err)
             if (err.response.status === 400) {
               this.$notifier.showMessage({
-                content: `ชื่อหัวข้อนี้เคยมีการสร้างแล้ว  ${this.form.name}`,
+                content: `data intent duplicate!  ${this.form.name}`,
                 color: 'red'
               })
             } else {
               this.$notifier.showMessage({
-                content: `มีบางอย่างผิดพลาด! ${err.response.status}`,
+                content: `something wrong! ${err.response.status}`,
                 color: 'red'
               })
             }
