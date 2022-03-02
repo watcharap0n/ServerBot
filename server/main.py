44fde9bef482d19d6aa68afb7c427f6fec7b1136
watcharap0n <wera.watcharapon@gmail.com>
2022-03-02 18:19:43 +0700

    update main.py version 2.6 branch review

--- server/main.py
+++ server/main.py
@@ -161,7 +161,7 @@ def custom_openapi():
         return app.openapi_schema
     openapi_schema = get_openapi(
         title="Mango-Server BOT",
-        version="2.1.0",
+        version="2.5.6",
         description=description,
         routes=app.routes,
     )
