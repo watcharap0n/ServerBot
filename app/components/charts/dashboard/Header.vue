3b0d71417073ba6bd5461ade58d207161e2d261b
watcharap0n <wera.watcharapon@gmail.com>
2022-03-04 17:50:21 +0700

    update components branch review

---
+++ app/components/charts/dashboard/Header.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <apexchart
+      height="350px"
+      type="line"
+      :options="options"
+      :series="series"
+  ></apexchart>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      options: {
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
+}
+</script>
