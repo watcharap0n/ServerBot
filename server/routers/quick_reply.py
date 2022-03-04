1111a51bcbe83234887669fef3d4cb84f1553b0e
watcharap0n <wera.watcharapon@gmail.com>
2022-03-04 13:17:17 +0700

    update logic quick_reply.py branch review

--- server/routers/quick_reply.py
+++ server/routers/quick_reply.py
@@ -13,21 +13,21 @@ collection = "quick_reply"
 
 
 async def check_quick_reply_duplicate(quick_reply: QuickReply):
-    items = await db.find(
-        collection=collection, query={"access_token": quick_reply.access_token}
+    item = await db.find(
+        collection=collection,
+        query={"access_token": quick_reply.access_token, "name": quick_reply.name}
     )
-    items = list(items)
-    for item in items:
-        if item["name"] == quick_reply.name:
-            raise HTTPException(
-                status_code=status.HTTP_400_BAD_REQUEST, detail="item name duplicate"
-            )
+    if item:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="item name duplicate"
+        )
     return quick_reply
 
 
 @router.get("/", response_model=List[TokenUser])
 async def get_quick_reply(
-    access_token: Optional[str] = None, current_user: User = Depends(get_current_active)
+        access_token: Optional[str] = None,
+        current_user: User = Depends(get_current_active)
 ):
     items = await db.find(collection=collection, query={"access_token": access_token})
     items = list(items)
@@ -36,8 +36,8 @@ async def get_quick_reply(
 
 @router.post("/create", response_model=TokenUser, status_code=status.HTTP_201_CREATED)
 async def create_quick_reply(
-    quick_reply: QuickReply = Depends(check_quick_reply_duplicate),
-    current_user: User = Depends(get_current_active),
+        quick_reply: QuickReply = Depends(check_quick_reply_duplicate),
+        current_user: User = Depends(get_current_active),
 ):
     item_model = jsonable_encoder(quick_reply)
     item_model = item_user(data=item_model, current_user=current_user)
@@ -48,7 +48,7 @@ async def create_quick_reply(
 
 @router.put("/query/update/{id}", response_model=UpdateQuickReply)
 async def update_query_quick_reply(
-    id: str, payload: UpdateQuickReply, current_user: User = Depends(get_current_active)
+        id: str, payload: UpdateQuickReply, current_user: User = Depends(get_current_active)
 ):
     data = jsonable_encoder(payload)
     query = {"_id": id}
@@ -64,7 +64,7 @@ async def update_query_quick_reply(
 
 @router.delete("/query/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_query_quick_reply(
-    id: str, current_user: User = Depends(get_current_active)
+        id: str, current_user: User = Depends(get_current_active)
 ):
     if (await db.delete_one(collection=collection, query={"_id": id})) == 0:
         raise HTTPException(
