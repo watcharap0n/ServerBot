51f830171a95c86e1cca64bc3fa05e926ecf2098
watcharap0n <wera.watcharapon@gmail.com>
2022-03-04 17:50:06 +0700

    update layout _channel.vue branch review

--- app/pages/callback/dashboard/_channel.vue
+++ app/pages/callback/dashboard/_channel.vue
@@ -1,25 +1,45 @@
 <template>
   <div>
-    <div v-if="$auth.loggedIn">
+    
+    <v-row>
+      <v-col sm="12"
+             cols="12"
+      >
+        <Header/>
+      </v-col>
+      <v-col sm="6"
+             cols="12"
+      >
+        <BodyLeft/>
+      </v-col>
 
+      <v-col sm="6"
+             cols="12"
+      >
+        <BodyRight/>
+      </v-col>
+    </v-row>
 
-
-    </div>
-
-    <div v-else>
-      <v-btn to="/authentication">SignIn</v-btn>
-    </div>
   </div>
 </template>
 
 <script>
-export default {
-  data(){
-    return{
+import Header from "@/components/charts/dashboard/Header";
+import BodyLeft from "@/components/charts/dashboard/BodyLeft";
+import BodyRight from "@/components/charts/dashboard/BodyRight";
 
-    }
+export default {
+  components: {
+    Header,
+    BodyLeft,
+    BodyRight
   },
-   async created() {
+
+  data() {
+    return {}
+  },
+
+  async created() {
     await this.$parent.$emit('routerHandle', this.$route.params);
   }
 }
