c15f5c7341ccf1236c68da19a3979b30efe5f8e3
watcharap0n <wera.watcharapon@gmail.com>
2022-03-08 13:10:06 +0700

    ีupdate form.py branch review

--- server/models/form.py
+++ server/models/form.py
@@ -7,5 +7,6 @@ from pydantic import BaseModel, Field
 class FormPage(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
     name: str
+    access_token: Optional[str] = None
     id_form: Optional[str] = None
-
+    
