e90e4f1446a4738f1cf2ec810044220f43a35715
watcharap0n <wera.watcharapon@gmail.com>
2022-03-08 09:26:17 +0700

    update model image_map.py branch review

--- server/routers/image_map.py
+++ server/routers/image_map.py
@@ -1,12 +1,14 @@
+import json
+from typing import Optional, Any
 from db import db
 from typing import List
 from oauth2 import get_current_active, User
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Body
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
 from linebot import LineBotApi
 from linebot.exceptions import LineBotApiError
-from models.image_map import ImageMap, TokenUser, UpdateImageMap
+from models.image_map import ImageMap, TokenUser, Mapping, UpdateImageMap
 from modules.item_static import item_user
 
 router = APIRouter()
@@ -14,7 +16,16 @@ router = APIRouter()
 collection = 'images_map'
 
 
-async def check_name_image(image: ImageMap):
+async def check_access_token(image: ImageMap):
+    try:
+        line_bot_api = LineBotApi(image.access_token)
+        line_bot_api.get_bot_info()
+        return image
+    except LineBotApiError as ex:
+        raise HTTPException(status_code=ex.status_code, detail=ex.message)
+
+
+async def check_name_image(image: ImageMap = Depends(check_access_token)):
     item = await db.find_one(collection=collection,
                              query={'access_token': image.access_token, 'name': image.name})
     if item:
@@ -22,10 +33,17 @@ async def check_name_image(image: ImageMap):
     return image
 
 
+def json_loads_encoder(content):
+    if content:
+        return json.loads(content)
+    pass
+
+
 @router.get('/', response_model=List[TokenUser])
 async def images_map(access_token: str):
     items = await db.find(collection=collection,
                           query={'access_token': access_token})
+    items = list(items)
     return items
 
 
@@ -39,16 +57,24 @@ async def image_map(access_token: str, id: str):
 @router.post('/create', response_model=TokenUser, status_code=status.HTTP_201_CREATED)
 async def add_image_map(image: ImageMap = Depends(check_name_image),
                         current_user: User = Depends(get_current_active)):
+    content = json_loads_encoder(image.content)
     item_model = jsonable_encoder(image)
-    await db.insert_one(collection=collection, data=item_model)
-    item_model = item_user(item_model, current_user)
-    item_store = TokenUser(**item_model)
+    item_model['size'] = content.get('size') if content else ''
+    item_model['areas'] = content.get('areas') if content else ''
+    item_model_mapping = Mapping(**item_model)
+    item_model_mapping = jsonable_encoder(item_model_mapping)
+    item_model_mapping = item_user(item_model_mapping, current_user)
+    await db.insert_one(collection=collection, data=item_model_mapping)
+    item_store = TokenUser(**item_model_mapping)
     return item_store
 
 
 @router.put('/query/update/{id}', response_model=UpdateImageMap)
 async def update_image_map(payload: UpdateImageMap, id: str):
+    content = json_loads_encoder(payload.content)
     item_model = jsonable_encoder(payload)
+    item_model['size'] = content.get('size') if content else ''
+    item_model['areas'] = content.get('areas') if content else ''
     query = {'_id': id}
     value = {'$set': item_model}
     if (await db.update_one(collection=collection, query=query, values=value)) == 0:
