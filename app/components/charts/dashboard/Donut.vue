0220319f9c195b727203bcd66366680ee04b1889
watcharap0n <wera.watcharapon@gmail.com>
2022-03-08 09:29:38 +0700

    update chart & rename

---
+++ app/components/charts/dashboard/Donut.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <apexchart
+        height="350px"
+        type="donut"
+        :options="options"
+        :series="series">
+    </apexchart>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: {},
+      series: [44, 55, 41, 17, 15],
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ app/components/charts/dashboard/LineChar.vue
@@ -0,0 +1,29 @@
+
+<template>
+  <apexchart
+      width="700px"
+      height="150px"
+      type="line"
+      :options="options"
+      :series="series"
+  ></apexchart>
+</template>
+
+
+<script>
+export default {
+  data () {
+    return {
+      options: {
+        xaxis: {
+          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998]
+        }
+      },
+      series: [{
+        name: 'series-1',
+        data: [30, 40, 45, 50, 49, 60, 70, 91]
+      }]
+    }
+  }
+}
+</script>
