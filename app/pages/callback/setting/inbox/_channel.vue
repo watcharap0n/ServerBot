b586f61881838455948561d1ae45238a9d9a219b
watcharap0n <wera.watcharapon@gmail.com>
2022-03-13 10:19:05 +0700

    add channel setting inbox

---
+++ app/pages/callback/setting/inbox/_channel.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+          sm="3"
+          cols="12"
+      >
+        <Settings/>
+      </v-col>
+
+      <v-col
+          sm="9"
+          cols="12">
+        <v-card flat>
+          <v-card-text>
+
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Settings from "@/components/app/Settings";
+
+export default {
+  components: {
+    Settings,
+  },
+  data() {
+    return {}
+  },
+  async created() {
+    await this.$parent.$emit('routerHandle', this.$route.params);
+    console.log(this.$route.params)
+  },
+  methods: {
+    selectItem() {
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
