eaddd24f72d7d4fb2030d19f50c06e959fe07ac5
watcharap0n <wera.watcharapon@gmail.com>
2022-03-13 10:27:58 +0700

    update modules mapping

--- server/modules/mapping.py
+++ server/modules/mapping.py
@@ -1,33 +1,33 @@
+from typing import Optional
 from linebot.models import ImagemapArea, ImagemapSendMessage, BaseSize, MessageImagemapAction, URIImagemapAction
 
 
-def mango_products():
-    template_image = ImagemapSendMessage(
-        base_url='https://sv1.picz.in.th/images/2020/12/02/j3VUfR.png',
-        alt_text='products',
-        base_size=BaseSize(height=1600, width=1040),
-        actions=[MessageImagemapAction(
-            text=i.get('data'),
-            area=ImagemapArea(
-                x=i['area']['x'],
-                y=i['area']['y'],
-                width=i['area']['w'],
-                height=i['area']['h'])
-        ) if i['type'] == 'message' else
-                 URIImagemapAction(
-                     link_uri=i['data'],
-                     area=ImagemapArea(
-                         x=i['area']['x'],
-                         y=i['area']['y'],
-                         width=i['area']['w'],
-                         height=i['area']['h']))
-                 for i in data]
-    )
-    return template_image
-
-
-data = [
-    {'type': 'message', 'area': {'x': 12, 'y': 54, 'w': 210, 'h': 123}, 'data': 'ok'},
-    {'type': 'message', 'area': {'x': 12, 'y': 54, 'w': 210, 'h': 123}, 'data': 'ok'},
-    {'type': 'uri', 'area': {'x': 12, 'y': 54, 'w': 210, 'h': 123}, 'data': 'ja'}
-]
+def mapping_image(base_url_image: Optional[str] = None,
+                  mapping: Optional[dict] = None):
+    if mapping.get('size') and mapping.get('areas'):
+        height = mapping['size']['height']
+        width = mapping['size']['width']
+        template_image = ImagemapSendMessage(
+            base_url=base_url_image if base_url_image else
+            'https://scdn.line-apps.com/n/channel_devcenter/img/fx/01_1_cafe.png',
+            alt_text='products',
+            base_size=BaseSize(height=height, width=width),
+            actions=[MessageImagemapAction(
+                text=i['action']['text'],
+                area=ImagemapArea(
+                    x=i['bounds']['x'],
+                    y=i['bounds']['y'],
+                    width=i['bounds']['width'],
+                    height=i['bounds']['height'])
+            ) if i['action']['type'] == 'message' else
+                     URIImagemapAction(
+                         link_uri=i['action']['uri'],
+                         area=ImagemapArea(
+                             x=i['bounds']['x'],
+                             y=i['bounds']['y'],
+                             width=i['bounds']['width'],
+                             height=i['bounds']['height']))
+                     for i in mapping['areas']]
+        )
+        return template_image
+    return False
