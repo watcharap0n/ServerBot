429fee37a77b45e6f3d78824b446fcf01c0eb1b5
watcharap0n <wera.watcharapon@gmail.com>
2022-03-13 10:25:32 +0700

    add router retrieve

---
+++ server/models/retrieve_data.py
@@ -0,0 +1,62 @@
+from db import PyObjectId
+from typing import Optional, Any, List
+from bson import ObjectId
+from pydantic import BaseModel, Field
+
+
+class Transaction(BaseModel):
+    transaction: Optional[dict] = None
+
+
+class Retrieve(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    transaction: Optional[dict] = None
+
+    class Config:
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "access_token": "access token long live",
+                "transaction":
+                    {
+                        'fname': 'watcharapon',
+                        'lname': 'weeraborirak',
+                        'name': 'kane'
+                    }
+
+            }
+        }
+
+
+class TokenUser(Retrieve):
+    access_token: Optional[str] = None
+    uid: Optional[str] = None
+    date: Optional[str] = None
+    time: Optional[str] = None
+
+    class Config:
+        schema_extra = {
+            "access_token": "token long live",
+            "uid": "generate token uid",
+            "date": "12/01/2022",
+            "time": "12:00:00",
+        }
+
+
+class UpdateRetrieve(BaseModel):
+    transaction: Optional[dict] = None
+
+    class Config:
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+            "example": {
+                "transaction":
+                    {
+                        'fname': 'watcharapon',
+                        'lname': 'weeraborirak',
+                        'name': 'kane'
+                    }
+
+            }
+        }
---
+++ server/routers/retrieve_data.py
@@ -0,0 +1,59 @@
+from bson import ObjectId
+from db import db, generate_token
+from typing import Optional, Any
+from starlette.responses import JSONResponse
+from modules.item_static import item_user
+from oauth2 import User, get_current_active
+from fastapi import APIRouter, HTTPException, Depends, status, Body
+
+router = APIRouter()
+
+collection = 'retrieve'
+
+
+@router.get('/')
+async def get_retrieve_data(access_token: str):
+    items = await db.find(collection=collection,
+                          query={"access_token": access_token},
+                          select_field={"_id": 0})
+    items = list(items)
+    return items
+
+
+@router.post("/create", status_code=status.HTTP_201_CREATED)
+async def create_retrieve(
+        access_token: str,
+        payload: Optional[Any] = Body(None),
+        current_user: User = Depends(get_current_active),
+):
+    ids = generate_token(engine=ObjectId())
+    payload['id'] = ids
+    payload['access_token'] = access_token
+    item_model = item_user(data=payload, current_user=current_user)
+    await db.insert_one(collection=collection, data=item_model)
+    del item_model["_id"]
+    return item_model
+
+
+@router.put("/query/update/{id}")
+async def update_query_intent(
+        id: str,
+        payload: Optional[Any] = Body(None),
+):
+    query = {"id": id}
+    values = {"$set": payload}
+
+    if (await db.update_one(collection=collection, query=query, values=values)) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"Retrieved not found {id}"
+        )
+    return payload
+
+
+@router.delete("/query/delete/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_query_intent(id: str):
+    if (await db.delete_one(collection=collection, query={"id": id})) == 0:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=f"Retrieved not found {id}"
+        )
+    return JSONResponse(status_code=status.HTTP_204_NO_CONTENT)
