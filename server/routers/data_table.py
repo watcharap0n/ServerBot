443e37cd4354f9aeb4b4bb7c57f2d3edb1dc38c2
watcharap0n <wera.watcharapon@gmail.com>
2022-03-14 13:23:11 +0700

    update oauth data_table.py

--- server/routers/data_table.py
+++ server/routers/data_table.py
@@ -14,7 +14,11 @@ collection = 'data_table'
 
 
 @router.get('/find', response_model=List[TokenUser])
-async def get_data_table(access_token: str, status: Optional[bool] = False):
+async def get_data_table(
+        access_token: str,
+        status: Optional[bool] = False,
+        current_user: User = Depends(get_current_active)
+):
     if status:
         data = await db.find(collection=collection, query={'access_token': access_token, 'status': status})
         data = list(data)
@@ -36,7 +40,8 @@ async def add_a_column(payload: ColumnDataTable,
 
 
 @router.put('/query/update/{id}', response_model=UpdateDataTable)
-async def update_a_column(id: str, payload: UpdateDataTable):
+async def update_a_column(id: str, payload: UpdateDataTable,
+                          current_user: User = Depends(get_current_active)):
     item_model = jsonable_encoder(payload)
     value = {'$set': item_model}
     query = {'_id': id}
@@ -47,7 +52,7 @@ async def update_a_column(id: str, payload: UpdateDataTable):
 
 
 @router.delete('/query/delete/{id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_a_column(id: str):
+async def delete_a_column(id: str, current_user: User = Depends(get_current_active)):
     if (await db.delete_one(collection=collection, query={'_id': id})) == 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail=f'column not found {id}')
