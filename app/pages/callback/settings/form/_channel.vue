f7997dba91739a20e0a8c1382b189c3056ea551f
watcharap0n <wera.watcharapon@gmail.com>
2022-03-15 09:18:39 +0700

    add channel custom form branch review

---
+++ app/pages/callback/settings/form/_channel.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+          sm="3"
+          cols="12"
+      >
+        <Settings/>
+      </v-col>
+
+      <v-col
+          sm="9"
+          cols="12">
+        <v-card
+            flat>
+
+          <v-card-title>
+            Custom Form
+          </v-card-title>
+          <v-divider></v-divider>
+
+          <v-card-text>
+            <v-container>
+
+              Here
+
+            </v-container>
+          </v-card-text>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Settings from "@/components/app/Settings";
+
+export default {
+  components: {
+    Settings,
+  },
+  data() {
+    return {
+      spin: false,
+    }
+  },
+  async created() {
+    await this.$parent.$emit('routerHandle', this.$route.params);
+
+  },
+  methods: {
+    selectItem() {
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
