952eea821ef41c2629e6d370e0fab82c4d56c57c
watcharap0n <wera.watcharapon@gmail.com>
2022-03-17 21:58:51 +0700

    add form branch review

---
+++ app/pages/callback/custom/form/_channel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      spin: false,
+    }
+  },
+
+  async created() {
+    await this.$parent.$emit('routerHandle', this.$route.params);
+    await this.getForm();
+  },
+
+  methods: {
+    async getForm() {
+      const path = `/form/find/${this.$route.query.q}`
+      await this.$axios.get(path)
+          .then((res) => {
+            console.log(res.data)
+          })
+          .catch((err) => {
+            console.error(err);
+          })
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
