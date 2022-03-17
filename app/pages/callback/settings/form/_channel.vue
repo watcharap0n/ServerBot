07d33dbef2d40677182ca67756a73a9810b05844
watcharap0n <wera.watcharapon@gmail.com>
2022-03-17 17:04:06 +0700

    callback form des settings _callback.vue

---
+++ app/pages/callback/settings/form/_channel.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <v-row>
+      <v-col
+          sm="3"
+          cols="12"
+      >
+        <Settings/>
+      </v-col>
+
+      <v-col
+          sm="9"
+          cols="12">
+        <v-card
+            flat>
+
+          <v-card-title>
+            Custom Form
+
+          </v-card-title>
+          <v-divider></v-divider>
+
+          <v-card-text>
+            <v-container>
+              <Form/>
+
+            </v-container>
+          </v-card-text>
+
+        </v-card>
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import Settings from "@/components/app/Settings";
+import Form from "@/components/settings/Form";
+
+export default {
+  components: {
+    Settings,
+    Form
+  },
+  data() {
+    return {
+      spin: false,
+    }
+  },
+  async created() {
+    await this.$parent.$emit('routerHandle', this.$route.params);
+
+  },
+  methods: {
+    selectItem() {
+
+    },
+  }
+}
+</script>
+
+<style scoped>
+</style>
