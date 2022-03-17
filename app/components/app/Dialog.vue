91b6575ea99aafc7894c71bd32b24589db7b9a12
watcharap0n <wera.watcharapon@gmail.com>
2022-03-17 12:26:52 +0700

    update Dialog branch review

--- app/components/app/Dialog.vue
+++ app/components/app/Dialog.vue
@@ -29,8 +29,8 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="red darken-1" text @click="close">ยกเลิก</v-btn>
-        <v-btn v-if="submitDialog" color="green" :loading="loadingDialog" text @click="submitDialog">ตกลง</v-btn>
+        <v-btn color="red darken-1" text @click="close">cancel</v-btn>
+        <v-btn v-if="submitDialog" color="green" :loading="loadingDialog" text @click="submitDialog">save</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
