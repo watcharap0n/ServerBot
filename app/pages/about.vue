22794e4b4959d620e64f7af60201035f45b32d39
watcharap0n <wera.watcharapon@gmail.com>
2022-03-24 11:43:26 +0700

    example draggable branch review

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
+    <draggable v-model="myArray">
+      <transition-group>
+        <div v-for="element in myArray" :key="element">
+          {{ element }}
+        </div>
+      </transition-group>
+    </draggable>
 
   </div>
 </template>
 
 <script>
-export default {}
+
+export default {
+  components: {
+    draggable,
+  },
+  data() {
+    return {
+      myArray: ['1', '2', '3']
+    }
+  }
+}
 </script>
 
 <style scoped>
