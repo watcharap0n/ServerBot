989b0d06253f3e326e96d839b220c26696e69a17
watcharap0n <wera.watcharapon@gmail.com>
2022-03-29 15:20:22 +0700

    update form custom branch review

--- app/pages/form/line/_channel.vue
+++ app/pages/form/line/_channel.vue
@@ -5,34 +5,39 @@
   >
     <div class="d-flex flex-column justify-space-between align-center">
       <v-img
-          :src="ImageURI"
+          :src="require('~/assets/images/mango-profile.jpg')"
           width="280"
       ></v-img>
 
       <v-card flat>
+
         <v-card-text>
           <div v-for="(v, k) in models">
-            <v-text-field
-                v-if="transaction.models[k].type_field === 'default'"
-                v-model="retrieves[v]"
-                color="success"
-                :label="transaction.models[k].text"
-                :outlined="transaction.models[k].outlined"
-                :rounded="transaction.models[k].rounded"
-                :solo="transaction.models[k].solo">
-            </v-text-field>
 
-            <v-select
-                v-if="transaction.models[k].type_field === 'select'"
-                :items="transaction.models[k].items_select"
-                v-model="retrieves[v]"
-                color="success"
-                item-color="success"
-                :outlined="transaction.models[k].outlined"
-                :rounded="transaction.models[k].rounded"
-                :solo="transaction.models[k].solo"
-            ></v-select>
+            <div v-if="transaction.models[k]">
+              <v-text-field
+                  v-if="transaction.models[k].type_field === 'default'"
+                  v-model="retrieves[v.value]"
+                  color="success"
+                  :label="transaction.models[k].text"
+                  :outlined="transaction.models[k].outlined"
+                  :rounded="transaction.models[k].rounded"
+                  :solo="transaction.models[k].solo">
+              </v-text-field>
+            </div>
 
+            <div v-if="transaction.models[k]">
+              <v-select
+                  v-if="transaction.models[k].type_field === 'select'"
+                  :items="transaction.models[k].items_select"
+                  v-model="retrieves[v.value].value"
+                  color="success"
+                  item-color="success"
+                  :outlined="transaction.models[k].outlined"
+                  :rounded="transaction.models[k].rounded"
+                  :solo="transaction.models[k].solo"
+              ></v-select>
+            </div>
           </div>
         </v-card-text>
 
@@ -44,7 +49,7 @@
               dark
               @click="save"
           >
-            Submit
+            submit
           </v-btn>
 
           <v-btn
@@ -52,32 +57,33 @@
               color="red"
               dark
           >
-            Cancel
+            cancel
           </v-btn>
         </v-card-actions>
 
       </v-card>
+
     </div>
-    <Overlay :overlay="overlay"/>
+    <overlay :overlay="overlay"/>
   </v-container>
 
 </template>
 
 <script>
-import Overlay from "@/components/app/Overlay";
+import overlay from "@/components/app/overlay";
 
 export default {
   layout: 'public',
 
   components: {
-    Overlay
+    overlay
   },
 
   data() {
     return {
       overlay: false,
       models: [],
-      accessToken: '',
+      accesstoken: '',
       transaction: {},
       retrieves: {},
     }
@@ -85,55 +91,47 @@ export default {
 
   async created() {
     this.overlay = true;
-    await this.getForm();
-    await this.getDataTable();
+    await this.getform();
+    await this.getdatatable();
     this.overlay = false;
   },
 
-  computed: {
-    ImageURI() {
-      return require('~/assets/images/mango-profile.jpg')
-    }
-  },
-
   methods: {
-    async getForm() {
+    async getform() {
       const path = `/form/find/${this.$route.params.channel}`
       await this.$axios.get(path)
           .then((res) => {
             this.transaction = res.data;
-            this.accessToken = res.data.access_token;
+            this.accesstoken = res.data.access_token;
           })
           .catch((err) => {
             console.error(err);
           })
     },
 
-    async getDataTable() {
-      let encoded = encodeURIComponent(this.accessToken);
+    async getdatatable() {
+      let encoded = encodeuricomponent(this.accesstoken);
       const path = `/data/table/find?access_token=${encoded}&default_field=false`
       await this.$axios.get(path)
           .then((res) => {
-            res.data.forEach((item) => {
-              this.models.push(item.value)
-            })
+            this.models = res.data;
           })
           .catch((err) => {
             console.error(err);
           })
     },
     async save() {
-      const path = `/retrieve/create?access_token=${this.accessToken}`;
+      const path = `/retrieve/create?access_token=${this.accesstoken}`;
       await this.$axios.post(path, this.retrieves)
           .then(() => {
             this.$swal.fire(
-                'Saved',
-                'You clicked the button!',
+                'saved',
+                'you clicked the button!',
                 'success'
             )
           })
           .catch((err) => {
-            this.$notifier.showMessage({
+            this.$notifier.showmessage({
               content: `something wrong | status code ${err.response.status}`,
               color: 'red'
             })
