b42919b860b4baf38b219acaaed0402c2a3b46f5
watcharap0n <wera.watcharapon@gmail.com>
2022-03-29 16:28:08 +0700

    public.vue

---
+++ app/layouts/public.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <v-app style="font-family: 'Prompt', sans-serif;" class="bg-gray-100 h-screen">
+      <Nuxt/>
+    </v-app>
+
+  </div>
+</template>
+
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+</style>
