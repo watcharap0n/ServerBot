ec3b87b88969af4fb23142858abbee08035b06db
watcharap0n <wera.watcharapon@gmail.com>
2022-04-17 21:41:23 +0700

    update layout lost connect branch review

--- app/layouts/empty.vue
+++ app/layouts/empty.vue
@@ -1,25 +1,42 @@
 <template>
   <div>
     <v-app :style="cssProps" style="font-family: 'Prompt', sans-serif;" class="bg-gray-100 h-screen">
+
       <Nuxt/>
+
+      <Overlay
+          :overlay="!onLine"
+          show-text="Please check your network!"
+          :show-refresh="true"
+      >
+      </Overlay>
     </v-app>
 
   </div>
 </template>
 
 <script>
+import Overlay from "@/components/app/Overlay";
+
 export default {
-  data(){
-    return{
+
+  components: {
+    Overlay,
+  },
+
+  data() {
+    return {
+      onLine: navigator.onLine,
       cssProps: {
-          backgroundImage: `url(${require('~/assets/images/Bg-login_bot.png')})`
-        }
+        backgroundImage: `url(${require('~/assets/images/Bg-login_bot.png')})`
+      }
 
     }
   },
-  computed:{
-    background(){
-      return{
+
+  computed: {
+    background() {
+      return {
         "background-image": require('~/assets/images/Bg-login_bot.png'),
 
       }
