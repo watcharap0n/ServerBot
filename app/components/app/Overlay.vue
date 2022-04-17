2f1b92bebd325ab76d4080bacafcee18fcb7b384
watcharap0n <wera.watcharapon@gmail.com>
2022-04-17 21:41:04 +0700

    update Overlay branch review

--- app/components/app/Overlay.vue
+++ app/components/app/Overlay.vue
@@ -1,17 +1,45 @@
 <template>
-   <v-overlay :value="overlay">
+  <v-overlay :value="overlay">
+
+    <div class="text-center">
       <v-progress-circular
-        indeterminate
-        size="64"
+          :color="color"
+          indeterminate
+          size="64"
       ></v-progress-circular>
-    </v-overlay>
+      <v-subheader>{{ showText }}</v-subheader>
+
+      <v-btn
+          :hidden="!showRefresh"
+          color="red" class="m-12"
+          @click="$router.go();"
+      >
+        try again
+      </v-btn>
+    </div>
+
+  </v-overlay>
 </template>
 
 <script>
-export default{
+export default {
   props: {
     overlay: {
       required: true
+    },
+    color: {
+      required: false,
+      type: String,
+      default: 'white',
+    },
+    showText: {
+      required: false,
+      type: String,
+      default: ''
+    },
+    showRefresh: {
+      required: false,
+      default: false,
     }
   }
 }
