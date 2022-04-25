f2f666be963f3116ef9460142e37ac7190ce4a31
watcharap0n <wera.watcharapon@gmail.com>
2022-04-25 08:21:02 +0700

    update layout dashboard branch review

--- app/pages/callback/dashboard/_channel.vue
+++ app/pages/callback/dashboard/_channel.vue
@@ -2,27 +2,7 @@
   <div>
 
     <v-row>
-      <v-col sm="6"
-             cols="12"
-      >
-        <v-card flat>
-          <v-card-text>
-            <Bar/>
-          </v-card-text>
-        </v-card>
-      </v-col>
-
-      <v-col sm="6"
-             cols="12">
-        <v-card flat>
-          <v-card-text>
-            <Donut/>
-          </v-card-text>
-        </v-card>
-      </v-col>
-
-      <v-col sm="6"
-             cols="12">
+      <v-col sm="12">
         <v-card flat>
           <v-card-text>
             <DataTable/>
@@ -30,21 +10,29 @@
         </v-card>
       </v-col>
 
-      <v-col sm="6"
-             cols="12">
+      <v-col sm="6">
         <v-card flat>
           <v-card-text>
-            <LineChar/>
+            <Bar/>
           </v-card-text>
         </v-card>
-        <br>
+      </v-col>
+
+      <v-col sm="6">
+        <v-card flat>
+          <v-card-text>
+            <Donut/>
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col sm="12">
         <v-card flat>
           <v-card-text>
             <LineChar/>
           </v-card-text>
         </v-card>
       </v-col>
-
     </v-row>
 
   </div>
